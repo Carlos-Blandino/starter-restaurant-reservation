f7904b403234a4cb5b94bd1022d9b32cda5d814e
Carlos Blandino <cablandino66@gmail.com>
2021-05-12 17:55:38 -0700

    adding tables functionality

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -5,11 +5,10 @@ const service = require("./reservations.service")
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
 
 async function list(req, res) {
- const data = await  service.list();
-     res.json({
-    data: data,
-  });
+    const data = await service.list()
+    res.json({data});
 }
+
 function hasProps(req, res, next){
     const {first_name,
         last_name,
